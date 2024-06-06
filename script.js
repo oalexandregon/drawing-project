61b85a31cfc078431e7dff4b80cd3c5c7449d4df
Alexandre Goncalves <avg1202@gmail.com>
2024-06-06 17:41:29 -0300

    Alterações nas funções para colocar no HTML

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 const getButton = document.querySelector('button');
-const participants = ['Gabriel', 'Christian', 'Rodrigo', 'Xandão', 'Pedro', 'Álvaro']
+const participants = ['Gabriel', 'Christian', 'Rodrigo', 'Xandão', 'Pedro', 'Álvaro'];
 
 
-function sortition () {
-    let drawedIndex = Math.floor(Math.random()* participants.length);
+
+function sortition() {
+    let drawedIndex = Math.floor(Math.random() * participants.length);
     return participants[drawedIndex];
 }
 
-function displayPerson () {
-    
+function displayPerson() {
+    const result = document.createElement('p');
+    const newH2 = document.createElement('h2');
+    const oldParagraph =  document.querySelector('.display > p');
+    const getH1 = document.querySelector('.display > h2');
+    const getDisplay = document.querySelector('.display');
+
+    oldParagraph.remove();
+
+   getDisplay.insertBefore(newH2, getH1);
+   getDisplay.insertBefore(result, getH1);
+
+
+
 }
+
+displayPerson();
 
 
 
 getButton.addEventListener('click', () => {
     const personDrawed = sortition();
-    console.log(personDrawed);
+    
 })
