1e8bcc38948a2869d269ea0048b01ec953948aba
Alexandre Goncalves <avg1202@gmail.com>
2024-06-06 18:29:11 -0300

    Projeto finalizado v.01

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
 const getButton = document.querySelector('button');
 const participants = ['Gabriel', 'Christian', 'Rodrigo', 'Xandão', 'Pedro', 'Álvaro'];
+const result = document.createElement('p');
+const newP = document.createElement('p');
 
 
 
 function sortition() {
     let drawedIndex = Math.floor(Math.random() * participants.length);
-    return participants[drawedIndex];
+    return { name: participants[drawedIndex], index: drawedIndex };
 }
 
-function displayPerson() {
-    const result = document.createElement('p');
-    const newH2 = document.createElement('h2');
-    const oldParagraph =  document.querySelector('.display > p');
-    const getH1 = document.querySelector('.display > h2');
-    const getDisplay = document.querySelector('.display');
-
-    oldParagraph.remove();
-
-   getDisplay.insertBefore(newH2, getH1);
-   getDisplay.insertBefore(result, getH1);
-
-
-
-}
-
-displayPerson();
-
-
+console.log(participants);
 
 getButton.addEventListener('click', () => {
-    const personDrawed = sortition();
-    
+
+    const getDisplay = document.querySelector('.display');
+    const { name, index } = sortition();
+
+    result.textContent = name;
+    result.className = 'result';
+    newP.textContent = 'O próximo consórcio é do: ';
+
+    getDisplay.children[1].remove();
+
+    getDisplay.insertBefore(newP, getButton);
+    getDisplay.insertBefore(result, getButton);
+
+    //Remove the draw participant: 
+
+    participants.splice(index, 1);
+    console.log(participants);
+
+    if (participants.length === 0) {
+        getButton.style.display = 'none';
+        newP.textContent = 'O último consórcio será do: ';
+    }
 })
